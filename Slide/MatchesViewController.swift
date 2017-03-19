7043f59380d5cb395d4e41168415bc2402ca19d9
bibek timalsina <timalsina.bibek@gmail.com>
2017-03-19 09:38:03 +0545

    matches sb

--- Slide/MatchesViewController.swift
+++ Slide/MatchesViewController.swift
@@ -9,27 +9,42 @@
 import UIKit
 
 class MatchesViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let data = [
+        ("Your Events", [#imageLiteral(resourceName: "coldcoffee.png"), #imageLiteral(resourceName: "fitness2.png"), #imageLiteral(resourceName: "Fitness.png")]),
+        ("Coffee", [#imageLiteral(resourceName: "stockpicround.png"), #imageLiteral(resourceName: "stockpicround1.png")]),
+        ("Fitness", [#imageLiteral(resourceName: "Fitness.png")]),
+        ("Gaming", [])
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MatchesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return data[indexPath.row].1.count > 0 ? 147 : 70
     }
-    */
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return data[indexPath.row].1.count > 0 ? 147 : 70
+    }
+}
 
+extension MatchesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MatchesTableViewCell", for: indexPath) as! MatchesTableViewCell
+        cell.titleLabel.text = data[indexPath.row].0
+        cell.images = data[indexPath.row].1
+        return cell
+    }
 }
