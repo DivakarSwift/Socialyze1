0658ab880078da69ab20f161635fc0c6946072fd
bibek timalsina <timalsina.bibek@gmail.com>
2017-07-19 11:08:23 +0545

    can set I am going

--- functions/index.js
+++ functions/index.js
@@ -19,3 +19,26 @@ admin.initializeApp(
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
+
+exports.iAmGoing = functions.https.onRequest((request, response) => {
+    const placeId = request.body.placeId;
+    const userId = request.body.userId;
+    const fbId = request.body.fbId;
+    const time = request.body.time;
+    const friends = request.body.friends; 
+    console.log(friends); 
+    
+    const url = "/Places/" + placeId + "/going/" + userId;
+    
+    admin.database().ref(url).set({
+        "time": time,
+        "fbId": fbId,
+        "userId": userId
+    }).then(snapshot => {
+        response.status(200).send(request.body.userId);
+    }).then(() => {
+        console.log("ddd");
+    });
+});
+
+
