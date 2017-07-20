820ee43663dcef46d7230166b4385799e40100c1
bibek timalsina <timalsina.bibek@gmail.com>
2017-07-20 07:10:10 +0545

    written functions

--- functions/index.js
+++ functions/index.js
@@ -5,52 +5,158 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.iAmGoing = functions.https.onRequest((request, response) => {
+    const url = constructIAmGoingUrl(request);
+    const data = constructIAmGoingData(request);
+
+    const promise = admin.database().ref(url).set(data)
+    handlePromise(request, response, promise);
+});
+
+exports.checkIn = functions.https.request((request, response) => {
+    checkIn(request, response, handlePromise);
+});
+
+exports.usedDeal = functions.https.request((request, response) => {
+    const url = constructUseDealUrl(request);
+    const data = request.body.time;
+    checkIn(request, response, (request, response, promise) => {
+        promise.
+            then(snapshot => {
+                console.log("checked in");
+                const promise = admin.database().ref(url).set(data)
+                handlePromise(request, response, promise);
+            });
+    });
+
+    handlePromise(request, response, promise);
+});
+
+function handlePromise(request, response, promise) {
+    promise
+        .then(snapshot => {
+            response.status(200).send("saved");
+        })
+        .then(() => {
+            sendPushNotificationToFacebookUsers(request);
+        });
+}
+
+// I am going
+function constructIAmGoingUrl(request) {
     const placeId = request.body.placeId;
+    const userId = request.body.userId;
+    const eventUid = request.body.eventUid;
+
+    return ("/Places/" + placeId + "/going/" + eventUid + "/" + userId);
+}
+
+function constructIAmGoingData(request) {
     const userId = request.body.userId;
     const fbId = request.body.fbId;
     const time = request.body.time;
-    const friendsFbId = request.body.friendsFbId;
-    const notificationTitle = request.body.notificationTitle;
-    const notificationBody = request.body.notificationBody;
-    const eventUid = request.body.eventUid
 
-    const url = "/Places/" + placeId + "/going/" + eventUid + "/" + userId;
-    
-    admin.database().ref(url).set({
+    let value = {
         "time": time,
         "fbId": fbId,
         "userId": userId
-    }).then(snapshot => {
-        response.status(200).send(request.body.userId);
-    })
-        .then(() => {
-            const payload = {
-                notification: {
-                    title: notificationTitle,
-                    body: notificationBody
-                }
-            };
+    };
+    return value;
+}
 
-            friendsFbId.forEach(function (element) {
+// checkin
+function constructCheckInUrl(request) {
+    const placeId = request.body.placeId;
+    const userId = request.body.userId;
 
-                admin.database().ref("user").orderByChild("profile/fbId").equalTo(element).once('value')
-                    .then(snapshot => {
-                        let token;
-                        snapshot.forEach(function (data) {
-                            token = data.val().fcmToken;
-                        });
-                        if (typeof token === 'string' || token instanceof String) {
-                            console.log(token);
-                            sendPushNotification(token, payload);
-                        }
-                    }).catch(function (error) {
-                        console.log(error);
-                    });
-            }, this);
+    return ("/Places/" + placeId + "/checkIn/" + userId);
+}
 
-            // console.log("ddd");
+function constructUserCheckInUrl(request) {
+    const userId = request.body.userId;
+    return ("/user/" + userId + "/checkIn");
+}
+
+function constructCheckInData(request) {
+    const userId = request.body.userId;
+    const fbId = request.body.fbId;
+    const time = request.body.time;
+
+    let value = {
+        "time": time,
+        "fbId": fbId,
+        "userId": userId
+    };
+    return value;
+}
+
+function constructUserCheckInData(request) {
+    const place = request.body.place;
+    const time = request.body.time;
+
+    let value = {
+        "time": time,
+        "place": place
+    };
+    return value;
+}
+
+function checkIn(request, response, callback) {
+    const url = constructCheckInUrl(request);
+    const data = constructCheckInData(request);
+
+    admin.database().ref(url).set(data)
+        .then(snapshot => {
+
+            const userCheckInUrl = constructUserCheckInUrl(request);
+            const userCheckInData = constructUserCheckInData(request);
+            const promise = admin.database().ref(userCheckInUrl).set(userCheckInData);
+            callback(request, response, promise);
         });
-});
+}
+
+// Use deal
+
+function constructUseDealUrl(request) {
+    const placeName = request.body.placeName;
+    const placeUid = request.body.placeUid;
+    const userId = request.body.userId;
+
+    return ("Places/" + placeName + "/deal/" + placeUid + "/users/" + userId + "/time");
+}
+
+// Other functions
+function getNotificationPayload(request) {
+    const notificationTitle = request.body.notificationTitle;
+    const notificationBody = request.body.notificationBody;
+
+    return {
+        notification: {
+            title: notificationTitle,
+            body: notificationBody
+        }
+    };
+}
+
+function sendPushNotificationToFacebookUsers(request) {
+    const facebookfriendsId = request.body.friendsFbId;
+    const payload = getNotificationPayload(request);
+
+    facebookfriendsId.forEach(function (element) {
+        admin.database().ref("user").orderByChild("profile/fbId").equalTo(element).once('value')
+            .then(snapshot => {
+                let token;
+                snapshot.forEach(function (data) {
+                    token = data.val().fcmToken;
+                });
+                if (typeof token === 'string' || token instanceof String) {
+                    console.log(token);
+                    sendPushNotification(token, payload);
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+    }, this);
+}
 
 function sendPushNotification(registrationToken, payload) {
     admin.messaging().sendToDevice(registrationToken, payload)
